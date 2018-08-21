5e885855edb6b2ac34d80d9ed7775edab556a881
Daniel P. Shannon <daniel@daniel.sh>
2018-08-21 05:15:23 -0400

    :hatching_chick: Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# `@ignota/susy.js`: JavaScript power tools for web layout.
+Susy.js is a JavaScript port of the powerful [Susy](https://github.com/oddbird/susy) layout toolkit for Sass. It makes the power and flexibility of Susy's venerable grid primitives available to your favorite CSS-in-JS solution---anything from Styled Components to inline React styles---so that you can build your own damn grids with your own damn tools.
+
+## Resources
+* [Changelog](CHANGELOG.md).
+* [Guidelines for contributing](CONTRIBUTING.md).
+* [Code of conduct](CONDUCT.md).
+* [GPL-3.0 license](LICENSE).
+
+## Installation
+```bash
+yarn add @ignota/susy.js
+```
---
+++ babel.config.js
@@ -0,0 +1,53 @@
+module.exports = api => ({
+    plugins: [
+        'autobind-class-methods',
+        ['@babel/proposal-class-properties', {
+            loose: true,
+        }],
+        ['@babel/proposal-decorators', {
+            legacy: true,
+        }],
+        '@babel/proposal-do-expressions',
+        '@babel/proposal-export-default-from',
+        '@babel/proposal-export-namespace-from',
+        '@babel/proposal-function-bind',
+        '@babel/proposal-function-sent',
+        '@babel/proposal-json-strings',
+        '@babel/proposal-logical-assignment-operators',
+        ['@babel/proposal-nullish-coalescing-operator', {
+            loose: true,
+        }],
+        '@babel/proposal-numeric-separator',
+        '@babel/proposal-optional-catch-binding',
+        ['@babel/proposal-optional-chaining', {
+            loose: true,
+        }],
+        ['@babel/proposal-pipeline-operator', {
+            proposal: 'minimal',
+        }],
+        '@babel/proposal-throw-expressions',
+        '@babel/syntax-dynamic-import',
+        '@babel/syntax-import-meta',
+        ['@babel/transform-classes', {
+            loose: true,
+        }],
+        api.env() === 'test' && 'istanbul',
+        'lodash',
+        ['module-resolver', {
+            root: api.env() === 'test'
+                ? ['./src', './spec']
+                : ['./src'],
+        }],
+        'ramda',
+    ].filter(Boolean),
+    presets: [
+        ['@babel/env', {
+            loose: true,
+            modules: 'commonjs',
+            targets: {
+                node: 'current',
+            },
+            useBuiltIns: 'usage',
+        }],
+    ],
+})
