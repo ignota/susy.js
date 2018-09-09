b23cab0ee96f2b7ef3a6844f6ef214c729c3fcc9
Daniel P. Shannon <daniel@daniel.sh>
2018-09-08 23:51:20 -0400

    :bug: Pass config to normalize.

--- src/susy/syntax-helpers.js
+++ src/susy/syntax-helpers.js
@@ -3,7 +3,7 @@ import { settings } from './settings'
 
 export const compile = ({ config, shorthand }) => {
   const normalizedConfig = config ? normalize(settings(config)) : normalize(settings())
-  const normalizedShorthand = normalize(shorthand)
+  const normalizedShorthand = normalize(shorthand, normalizedConfig)
 
   return Object.assign({}, normalizedConfig, normalizedShorthand)
 }
